ec19529fdf74431e19f49aa1bf743d11de09211a
“HarryB312” <harrison.boswell@gmail.com>
2025-04-09 22:34:12 -0400

    Added practice functions

---
+++ Functions/app.js
@@ -0,0 +1,100 @@
+//REUSABLE PROCEDURES//
+//function funcName(){
+//  do something
+// }
+
+// function singSong(){
+//     console.log('DO')
+//     console.log('RE')
+//     console.log('MI')
+// }
+
+// singSong()
+
+// function greet(name){ //perameter
+//     console.log(`Hello, ${name}!`)
+// }
+
+// greet('harrison') //argument 
+
+
+
+//Multiple Arguments//
+
+// function greet(firstName, lastName){ //perameter
+//     console.log(`Hello, ${firstName} ${lastName[0]}!`)
+// }
+
+// greet('Harrison', 'Boswell')
+
+
+// function repeat(str, howMany){
+//     let result = '';
+//     for (let i = 0; i < howMany; i++){
+//         result += str;
+//     }
+//     console.log(result)
+// }
+
+
+// function isSnakeEyes(die1, die2){
+//     if (die1 === 1 && die2 === 1){
+//         console.log('Snake Eyes!')
+//     }else {
+//         console.log('Not Snake Eyes!')
+//     }
+// }
+
+// isSnakeEyes(1,1)
+
+
+
+// RETURN // capute and save output to be resused//
+function add(x, y){
+    if (typeof x !== 'number' || typeof y !== 'number'){
+        return false;
+    }
+    return x + y;
+}
+
+function multiply(x, y){
+    return x * y;
+}
+
+multiply(5,5)
+
+function isShortsWeather(temp){
+    if (temp >= 75){
+        return true;
+    }return false;
+}
+
+isShortsWeather(88);
+
+
+function lastElement(array){
+    if (array.length === 0){
+        return null;
+    }else {
+        return array[array.length -1]
+    }
+}
+
+
+function capitalize(str){
+    if (!str) return '';
+    return str[0].toUpperCase() + str.slice(1) //This takes a substring starting from index 1 to the end of the string:/
+}
+
+capitalize('harrison')
+
+
+function showDay(num){
+    const days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+    if (num < 1 || num > 7){
+        return null;
+    }else {
+        return days[num - 1]
+    }
+    
+}
