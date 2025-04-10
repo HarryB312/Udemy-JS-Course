753de9504a430b5d1a705170bde82f13e8f5b784
“HarryB312” <harrison.boswell@gmail.com>
2025-04-10 19:01:30 -0400

    N/A

--- Functions/app.js
+++ Functions/app.js
@@ -50,51 +50,55 @@
 
 
 // RETURN // capute and save output to be resused//
-function add(x, y){
-    if (typeof x !== 'number' || typeof y !== 'number'){
-        return false;
-    }
-    return x + y;
-}
+// function add(x, y){
+//     if (typeof x !== 'number' || typeof y !== 'number'){
+//         return false;
+//     }
+//     return x + y;
+// }
 
-function multiply(x, y){
-    return x * y;
-}
+// function multiply(x, y){
+//     return x * y;
+// }
 
-multiply(5,5)
+// multiply(5,5)
 
-function isShortsWeather(temp){
-    if (temp >= 75){
-        return true;
-    }return false;
-}
+// function isShortsWeather(temp){
+//     if (temp >= 75){
+//         return true;
+//     }return false;
+// }
 
-isShortsWeather(88);
-
-
-function lastElement(array){
-    if (array.length === 0){
-        return null;
-    }else {
-        return array[array.length -1]
-    }
-}
+// isShortsWeather(88);
 
 
-function capitalize(str){
-    if (!str) return '';
-    return str[0].toUpperCase() + str.slice(1) //This takes a substring starting from index 1 to the end of the string:/
-}
+// function lastElement(array){
+//     if (array.length === 0){
+//         return null;
+//     }else {
+//         return array[array.length -1]
+//     }
+// }
 
-capitalize('harrison')
+
+// function capitalizeWord(word){
+//     return word[0].toUpperCase() + word.slice(1) //This takes a substring starting from index 1 to the end of the string:/
+// }
+
+// capitalizeWord('harrison')
 
 
-function showDay(num){
-    const days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
-    if (num < 1 || num > 7){
-        return null;
-    }else {
-        return days[num - 1]
-    }
+// function showDay(num){
+//     const days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+//     if (num < 1 || num > 7){
+//         return null;
+//     }else {
+//         return days[num - 1]
+//     }
     
-}
+// }
+
+
+
+
+
