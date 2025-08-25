152b193a5efda8773650513042bafbd827cd6515
HarrB312 <harrison.boswell@mail.com>
2025-08-24 23:03:04 -0400

    started the better sign up form

--- Practice/Forms/forms/src/BetterSignUpForm.jsx
+++ Practice/Forms/forms/src/BetterSignUpForm.jsx
@@ -1,34 +1,18 @@
 import { useState } from "react"
 
 function BetterSignUpForm(){
-    const [formData, setFromData] = useState({firstName: '', lastName: ''})
-    const handleChange = (evt) =>{
-        const changeField = evt.target.name;
-        const newValue = evt.target.value;
-    }
-    
-        return(
-        <div>
-            <label htmlFor="firstname">First Name</label>
-            <input 
-                type="text" 
-                placeholder="First Name" 
-                value={formData.firstname}
-                onChange={handleChange}
-                name="firstname"
-                id="firstname"/>
-            <label htmlFor="lastname">Last Name</label>
-            <input 
-                type="text" 
-                placeholder="Last Name" 
-                value={formData.lastName}
-                onChange={handleChange}
-                name="lastname"
-                id="lastname"/>
-            <button>submit</button>
-            
+    return(
+      <div>
+        <input className="firstname"
+           type="text" 
+           placeholder="First Name"
+            />
+        <input className="lastname"
+            type="text" 
+            placeholder="Last Name"
+            />
+            <button>Submit</button>
         </div>
-        
     )
 }
 
