c76f7f7b81cac7cd04287767284c7ae7f63e375e
HarrB312 <harrison.boswell@mail.com>
2025-08-26 21:45:49 -0400

    added password input

--- Practice/Forms/forms/src/BetterSignUpForm.jsx
+++ Practice/Forms/forms/src/BetterSignUpForm.jsx
@@ -1,19 +1,50 @@
 import { useState } from "react"
 
-function BetterSignUpForm(){
+  function BetterSignUpForm(){
+      const [formData, setFromData] = useState({firstName: '', lastName: '', password: ''});
+      const handleChange = (evt) =>{
+        const changeField = evt.target.name;
+        const newValue = evt.target.value;
+        setFromData(currData => {
+          currData[changeField] = newValue
+          return {...currData}
+        })
+      };
+      const handleSubmit = ()=>{
+        console.log(formData)
+      }
     return(
       <div>
-        <input className="firstname"
-           type="text" 
-           placeholder="First Name"
-            />
-        <input className="lastname"
-            type="text" 
-            placeholder="Last Name"
-            />
-            <button>Submit</button>
-        </div>
-    )
+        <label htmlFor="firstname"></label>
+        <input
+          type="text" 
+          placeholder="First Name"
+          value={formData.firstName}
+          onChange={handleChange}
+          name="firstName"
+          id="firstname"
+          />
+        <label htmlFor="lastname"></label>
+        <input 
+          type="text" 
+          placeholder="Last Name"
+          value={formData.lastName}
+          onChange={handleChange}
+          name="lastName"
+          id="lastname"
+          />
+          <label htmlFor="password"></label>
+          <input 
+          type="password" 
+          placeholder="Password"
+          value={formData.password}
+          onChange={handleChange}
+          name="password"
+          id="password"
+          />
+        <button onClick={handleSubmit}>Submit</button>
+      </div>
+  )
 }
 
-export default BetterSignUpForm
+  export default BetterSignUpForm
