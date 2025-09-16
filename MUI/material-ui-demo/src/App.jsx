483eb30916146e637ff44939af244098b04f388a
HarrB312 <harrison.boswell@mail.com>
2025-09-15 20:11:54 -0400

    updated MUI files

--- MUI/material-ui-demo/src/App.jsx
+++ MUI/material-ui-demo/src/App.jsx
@@ -1,14 +1,18 @@
 import './App.css'
 import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
 import Counter from './Counter';
+import TodoList from './TodoList';
 
 function App() {
   return (
     <div>
-      <Button color='success' variant="contained" onClick={()=>{alert('Hello')}}>Contained</Button>
-      <Counter />
+      <CssBaseline />
+      {/* <Button color='success' variant="contained" onClick={()=>{alert('Hello')}}>Contained</Button> */}
+      {/* <Counter /> */}
+      <TodoList />
     </div>
   )
 }
 
-export default App
+export default App;
